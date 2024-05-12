f0c631c733e88a184030c66f468f80c93fc54a23
githubärjfovo <githubrjf12138@gmail.com>
2024-05-13 00:04:21 +0800

    视频播放器增加中文翻译

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -2,83 +2,52 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
-    <name>FinishedPage</name>
-    <message>
-        <location filename="../FinishedPage.qml" line="46"/>
-        <source>The update is complete and we recommend that you restart your computer.</source>
-        <translation>更新已完成，建议您重新启动计算机。</translation>
-    </message>
-    <message>
-        <location filename="../FinishedPage.qml" line="53"/>
-        <source>Update failed</source>
-        <translation>更新失败</translation>
-    </message>
-    <message>
-        <location filename="../FinishedPage.qml" line="65"/>
-        <source>Exit</source>
-        <translation>退出</translation>
-    </message>
-    <message>
-        <location filename="../FinishedPage.qml" line="72"/>
-        <source>Reboot</source>
-        <translation>重新启动</translation>
-    </message>
-</context>
-<context>
-    <name>HomePage</name>
-    <message>
-        <location filename="../HomePage.qml" line="65"/>
-        <source>Checking for updates...</source>
-        <translation>正在检查更新...</translation>
-    </message>
-    <message>
-        <location filename="../HomePage.qml" line="80"/>
-        <source>Check for update failure</source>
-        <translation>检查更新失败</translation>
-    </message>
-    <message>
-        <location filename="../HomePage.qml" line="123"/>
-        <source>Recheck</source>
-        <translation>重新检查</translation>
-    </message>
-</context>
-<context>
-    <name>UpdatePage</name>
-    <message>
-        <location filename="../UpdatePage.qml" line="64"/>
-        <source>Package updates are available</source>
-        <translation>有可用的软件更新</translation>
-    </message>
-    <message>
-        <location filename="../UpdatePage.qml" line="70"/>
-        <source>Your system is up to date</source>
-        <translation>您的系统已是最新</translation>
-    </message>
-    <message>
-        <location filename="../UpdatePage.qml" line="76"/>
-        <source>Current Version: %1</source>
-        <translation>当前版本: %1</translation>
-    </message>
-    <message>
-        <location filename="../UpdatePage.qml" line="172"/>
-        <source>Update now</source>
-        <translation>立即更新</translation>
-    </message>
-</context>
-<context>
-    <name>UpdatingPage</name>
-    <message>
-        <location filename="../UpdatingPage.qml" line="43"/>
-        <source>Updating, please wait</source>
-        <translation>正在更新，请稍候</translation>
-    </message>
-</context>
-<context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="35"/>
-        <source>Software update</source>
-        <translation>软件更新</translation>
+        <location filename="../qml/main.qml" line="46"/>
+        <source>Video Player</source>
+        <translation>视频播放器</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="53"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="65"/>
+        <source>Select file</source>
+        <translation>选择视频文件</translation>
+    </message>
+</context>
+<context>
+    <name>PlayListItem</name>
+    <message>
+        <location filename="../qml/PlayListItem.qml" line="46"/>
+        <source>Play</source>
+        <translation>播放</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayListItem.qml" line="46"/>
+        <source>Remove from list</source>
+        <translation>从列表中移除</translation>
+    </message>
+</context>
+<context>
+    <name>PlayList</name>
+    <message>
+        <location filename="../qml/PlayList.qml" line="46"/>
+        <source>Playlist is empty</source>
+        <translation>空播放列表</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayList.qml" line="46"/>
+        <source>Remove from list</source>
+        <translation>从列表中移除</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayList.qml" line="46"/>
+        <source>Remove from list</source>
+        <translation>从列表中移除</translation>
     </message>
 </context>
 </TS>
